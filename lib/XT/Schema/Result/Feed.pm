7bf56673ecac7d8289716473af88beff5067d5a4
Kent Fredric <kentfredric@gmail.com>
2013-02-13 09:15:59 +1300

    Add a Feed which can provide Articles

--- lib/XT/Schema/Result/Feed.pm
+++ lib/XT/Schema/Result/Feed.pm
@@ -1,26 +1,21 @@
 use strict;
 use warnings;
 
-package XT::Schema::Result::Article;
+package XT::Schema::Result::Feed;
 
 use Moose;
 
 extends 'DBIx::Class::Core';
 
-__PACKAGE__->table('article');
+__PACKAGE__->table('feed');
 __PACKAGE__->add_column(
   id => {
     is_numeric         => 1,
     is_nullable        => 0,
     is_auto_increment  => 1,
-    sequence           => 'article_id_seq',
+    sequence           => 'feed_id_seq',
     retrieve_on_insert => 1,
     auto_nextval       => 1,
-  },
-  source_feed => {
-    is_numeric         => 1,
-    is_nullable        => 1,
-    retrieve_on_insert => 1,
   },
   submitting_user => {
     is_numeric         => 1,
@@ -29,18 +24,12 @@ __PACKAGE__->add_column(
   },
 );
 __PACKAGE__->set_primary_key('id');
-__PACKAGE__->add_unique_constraint( article_id_unique => ['id'] );
+__PACKAGE__->add_unique_constraint( feed_id_unique => ['id'] );
 __PACKAGE__->add_relationship(
-  article_has_feed => Feed => {
-    'foreign.id' => 'self.source_feed',
-  }
-);
-__PACKAGE__->add_relationship(
-  article_has_submitting_user => User => {
+  feed_has_submitting_user => User => {
     'foreign.id' => 'self.submitting_user',
   }
 );
-
 __PACKAGE__->meta->make_immutable;
 
 1;
