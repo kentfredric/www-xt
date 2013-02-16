40146be51d1f44e0d424b38f4de37d12e376236c
Kent Fredric <kentfredric@gmail.com>
2013-02-17 10:23:53 +1300

    cleanup Schema.pm , make schema_version independent from $VERSION

--- lib/XT/Schema.pm
+++ lib/XT/Schema.pm
@@ -3,14 +3,11 @@ use warnings;
 
 package XT::Schema;
 
-use Moose;
-
 use parent 'DBIx::Class::Schema';
+
+sub schema_version { 1 }
 
 __PACKAGE__->load_namespaces();
 __PACKAGE__->load_components('Schema::Config');
-
-#__PACKAGE__->backup_directory( $backup );
-#__PACKAGE__->upgrade_directory($upgrades);
 
 1;
