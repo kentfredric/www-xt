63861fc8a8825b41a8bae551572d84bdb05f3ad2
Kent Fredric <kentfredric@gmail.com>
2013-02-17 10:21:51 +1300

    add a custom Candy.pm

---
+++ lib/XT/Schema/Candy.pm
@@ -0,0 +1,29 @@
+
+use strict;
+use warnings;
+
+package XT::Schema::Candy;
+
+use parent 'DBIx::Class::Candy';
+
+# Class -> table translator.
+# mostly because autopluralisation sucks.
+sub import {
+  my ($class) = shift;
+  @_ = ( $class, -autotable => 1, @_ );
+  goto \&DBIx::Class::Candy::import;
+}
+
+sub base {
+  'DBIx::Class::Core';
+}
+
+sub gen_table {
+  my ( $self, $class ) = @_;
+  my $t    = $class;
+  $t =~ s/^XT::Schema::Result:://;
+  $t =~ s/::/_/g;
+  return lc($t);
+}
+
+1;
