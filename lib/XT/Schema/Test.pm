942ed5523f98c0e7d3e91299dc75d0db5b6e516d
Kent Fredric <kentfredric@gmail.com>
2013-02-17 13:34:21 +1300

    Fix/tidy Test, so it no longer makes files called GLOB(0xDEADBEEF) that are sqlite databases

--- lib/XT/Schema/Test.pm
+++ lib/XT/Schema/Test.pm
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 use v5.10;
- 
+
 package XT::Schema::Test;
 
 use File::Temp qw( tempdir tempfile );
@@ -10,30 +10,35 @@ use Path::Tiny qw( path );
 use File::Spec;
 use XT::Schema;
 
-sub tmpdir_name { 
-    return 'XT_test';
+sub tmpdir_name {
+  return 'XT_test';
 }
 
 sub scratch {
-    return state $scratch = do {
-      my $p = path( File::Spec->tmpdir )->child($_[0]->tmpdir_name);
-      $p->mkpath;
-      $p;
-    };
+  return state $scratch = do {
+    my $p = path( File::Spec->tmpdir )->child( $_[0]->tmpdir_name );
+    $p->mkpath;
+    $p;
+  };
 }
+
 sub xt_tempdir {
-    return state $dir = do {
-        tempdir( 'XT.XXXXXX' , DIR => $_[0]->scratch()->absolute->stringify , CLEANUP => 1 );
-    };
+  return state $dir = do {
+    tempdir( 'XT.XXXXXX', DIR => $_[0]->scratch()->absolute->stringify, CLEANUP => 1 );
+  };
 }
+
 sub db_file {
-    return tempfile( "XT.XXXXXX", SUFFIX => '.sqlite', DIR => $_[0]->xt_tempdir() )
+  state $files = [];
+  my ( $tempfile, $filename ) = tempfile( "XT.XXXXXX", SUFFIX => '.sqlite', DIR => $_[0]->xt_tempdir() );
+  push @{$files}, $tempfile;
+  return path($filename)->absolute->stringify;
 }
 
 sub tdb {
-    my $instance = XT::Schema->connect('dbi:SQLite:' . $_[0]->db_file );
-    $instance->deploy();
-    return $instance;
+  my $instance = XT::Schema->connect( 'dbi:SQLite:' . $_[0]->db_file );
+  $instance->deploy();
+  return $instance;
 }
 
 1;
